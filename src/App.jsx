5d261db16a393e058fc4f9f37779bb4f2557693f
Margarita <154820343+CaptainKirioto@users.noreply.github.com>
2024-09-13 15:06:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import s from "./App.module.css";
+
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+
+function App() {
+  return (
+    <div className={s.wrapper}>
+      <h1 className={s.title}>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import Contact from "../Contact/Contact";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filterSlice";
+
+//  { contacts, handleDelete }
+
+const ContactList = ({ handleDelete }) => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul>
+      {filteredContacts.map((contact) => {
+        return (
+          <li key={contact.id}>
+            <Contact
+              id={contact.id}
+              name={contact.name}
+              number={contact.number}
+              handleDelete={handleDelete}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
